b7ef66332e19688b43c8b905c6258787f537a481
atevilson <freitas.atevilson@gmail>
2024-02-17 00:58:26 -0300

    sr 001 - update main

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def limpeza_dados(dados):
-    return dados.drop_duplicates()
+from data.gerador_dados import gerar_dados_falsos
+from utils.preprocessamento import limpar_dados
+from model.recomendacao_model import treinar_modelo
+
+# gerar dados fictícios
+dados = gerar_dados_falsos()
+
+# pré-processamento
+dados = limpar_dados(dados)
+
+# treinamento do modelo
+modelo, previsoes = treinar_modelo(dados)
